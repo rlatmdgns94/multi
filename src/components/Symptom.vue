44cca48d6ba8960ac472cb66c7a71b32e0de92f4
김승훈 <rlatmdgns94@naver.com>
2019-03-13 02:36:29 +0900

    당뇨 프로그램  증상 선택 페이지 작업 진행중

--- src/components/Symptom.vue
+++ src/components/Symptom.vue
@@ -53,14 +53,98 @@
               </label>
             </li>
           </ul>
-          <div>
-            <ul class="diabetes_guide">
-              <li>
-                <span class="step">Step3</span>
-              </li>
-              <li></li>
-              <li></li>
+          <div class="diabetes_option_box">
+            <ul class="diabetes_guide diabetes_guide_v1">
+                <li><span class="step">Step3</span></li>
+                <li><p class="guide_title">고객님의 <em>식단 프로그램</em>을 확인해주세요.</p></li>
+                <li><p>당뇨는 적절한 식사요법이 치료의 기본 요소입니다.</p></li>
             </ul>
+            <ul class="diabetes_option">
+                <li>
+                    <span class="diabetes_option_title">하루에</span>
+                    <div class="option_box">
+                      <input type="radio" name="option_meals" value="a" id="option_meals" class="option_selected">
+                      <label for="option_meals" class="diabetes_radio_btn"><span></span>1끼</label>
+                    </div>
+                    <div class="option_box">
+                      <input type="radio" name="option_meals" value="a" id="option_meals2" class="option_selected">
+                      <label for="option_meals2" class="diabetes_radio_btn"><span></span>2끼</label>
+                    </div>
+                </li>
+                <li>
+                    <span class="diabetes_option_title">한주에</span>
+                    <div class="option_box">
+                      <input type="radio" name="option_meals" value="a" id="option_meals" class="option_selected">
+                      <label for="option_meals" class="diabetes_radio_btn"><span></span>3일</label>
+                    </div>
+                    <div class="option_box">
+                      <input type="radio" name="option_meals" value="a" id="option_meals2" class="option_selected">
+                      <label for="option_meals2" class="diabetes_radio_btn"><span></span>7일</label>
+                    </div>
+                </li>
+                <li>
+                    <span class="diabetes_option_title">총 몇주</span>
+                    <div class="option_box">
+                      <input type="radio" name="option_meals" value="a" id="option_meals" class="option_selected">
+                      <label for="option_meals" class="diabetes_radio_btn"><span></span>4주</label>
+                    </div>
+                    <div class="option_box">
+                      <input type="radio" name="option_meals" value="a" id="option_meals2" class="option_selected">
+                      <label for="option_meals2" class="diabetes_radio_btn"><span></span>8주</label>
+                    </div>
+                </li>
+            </ul>
+            <ul class="diabetes_guide diabetes_guide_v1">
+                <li><span class="step">Step4</span></li>
+                <li><p class="guide_title">식단의 <em>옵션을</em>을 선택해주세요.</p></li>
+                <li><p>당뇨는 적절한 식사요법이 치료의 기본 요소입니다.</p></li>
+            </ul>
+            <ul class="main_menu_option">
+               <li>
+                    <div class="option_box">
+                      <img src="../assets/img/option1.png" alt="">
+                      <input type="radio" name="option_meals" value="a" id="option_meals" class="option_selected">
+                      <label for="option_meals" class="diabetes_radio_btn"><span></span>메인식단</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="option_box">
+                      <img src="../assets/img/option2.png" alt="">
+                      <input type="radio" name="option_meals" value="a" id="option_meals" class="option_selected">
+                      <label for="option_meals" class="diabetes_radio_btn"><span></span>메인+반찬</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="option_box">
+                      <img src="../assets/img/option3.png" alt="">
+                      <input type="radio" name="option_meals" value="a" id="option_meals" class="option_selected">
+                      <label for="option_meals" class="diabetes_radio_btn"><span></span>메인+반찬+간식</label>
+                    </div>
+                </li>
+            </ul>
+            <ul class="diabetes_guide diabetes_guide_v1">
+                <li><span class="step">Step5</span></li>
+                <li><p class="guide_title">매주 배송 받을 <em>요일</em>을 선택해주세요.</p></li>
+                <li><p>당뇨는 적절한 식사요법이 치료의 기본 요소입니다.</p></li>
+            </ul>
+            <ul class="early_serious_chronic">
+            <li>
+              <label>
+                <input type="radio" name="option_recommendation" value="early" class="button_like">
+                <span class="symptom_button">매주 화요일</span>
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type="radio" name="option_recommendation" value="early" class="button_like">
+                <span class="symptom_button">매주 목요일</span>
+              </label>
+            </li>
+          </ul>
+          </div>
+          <div>
+            <p>일반당뇨 - 초기당뇨 맞춤당 프로그램</p><p>159,000 원</p>
+
           </div>
         </form>
       </div>
@@ -94,6 +178,12 @@
     background: url("../assets/img/diabetes_guide_arrow.png") no-repeat;
     margin-top: 62px;
   }
+  &.diabetes_guide_v1{
+        background: #fff;
+        &::after{
+          height:0;
+        }
+  }
   li {
     margin-top: 7px;
     &:first-child {
@@ -117,7 +207,7 @@
   }
 }
 .program_option {
-  padding-top: 60px;
+  padding: 60px 0;
   background: #fcf9ed;
   font-family: "SDKukdetopokki";
   .early_serious_chronic {
@@ -146,6 +236,82 @@
     }
   }
 }
+ .diabetes_option_box{
+        width: 1400px;
+        margin: 0 auto;
+        overflow: hidden;
+        background: #fff;
+        border: 1px solid #707070;
+        margin-top: 100px;
+        margin-bottom: 50px;
+        padding-bottom: 100px;
+    }
+    .diabetes_option{
+      text-align: center;
+      li{
+        position: relative;
+        display: inline-block;
+        text-align: left;
+        margin-left: 180px;
+        &:first-child{
+          margin-left: 0;
+        }
+        &:after{
+          position: absolute;
+          right:-50%;
+          top:0;
+          content: "+";
+          font-size:30px;
+        }
+        &:last-child:after{
+          font-size: 0;
+        }
+      }
+      .diabetes_option_title{
+        display: block;
+        padding: 4px 55px;
+        margin-bottom: 60px;
+        color:#fff;
+        font-size:28px;
+        background:#85AF4B;
+      }
+     
+        
+    }
+     .option_box{
+        margin-top:5px;
+        text-align: center;
+        img{display: block; margin-bottom:38px;}
+      }
+    .option_selected{
+            display: none;
+        }
+     .diabetes_radio_btn{
+          font-size:22px;
+          text-align: center;
+            span{
+                display: inline-block;
+                width: 29px;
+                height:29px;
+                margin-top: 2px;
+                margin-right:15px;
+                vertical-align: top;
+                background: url('../assets/img/check.png') no-repeat;
+            }
+        }
+        .option_selected:checked+.diabetes_radio_btn span{
+            background: url('../assets/img/checked.png') no-repeat;
+        }
+    .main_menu_option{
+      text-align: center;
+      li{
+        display: inline-block;
+        margin-right:220px;
+        &:last-child{
+          margin:0;
+        }
+      }
+    }
 </style>
 
 <script>
